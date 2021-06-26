a5d55528f9dd178b0d316e36eb10cfb252afca24
kishansinha181 <kishan.sinha18@gmail.com>
2021-06-26 15:32:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return (
+    <ChatEngine
+        height = "100vh"
+        projectID = "21dfe8be-2eab-4fd5-9c33-61da73d2adf4"
+        userName = ""
+        userSecret = ""
+
+
+    />
+    )
+}
+
+export default App;
